6475c2831580bd0af16dc4e102f3e0c0d5f3c4f4
VI1N1 <130185623+VI1N1@users.noreply.github.com>
2023-04-29 22:27:00 -0400

    Add files via upload

---
+++ db_portal.sql
@@ -0,0 +1,89 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP DATABASE IF EXISTS `db_portal`;
+CREATE DATABASE IF NOT EXISTS `db_portal` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_portal`;
+
+CREATE TABLE `login` (
+  `codigo_login` int(11) NOT NULL,
+  `nome_completo_login` varchar(100) NOT NULL,
+  `nome_login` varchar(50) NOT NULL,
+  `senha_login` varchar(32) NOT NULL,
+  `tipo_login` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `usuario` (
+  `codigo_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(100) NOT NULL
+  `cpf_usuario` bigint(11) NOT NULL,
+  `num_celular` bigint(11) NOT NULL,
+  `codigo_login` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `veiculo` (
+  `codigo_veiculo` int(11) NOT NULL,
+  `marca_veiculo` varchar(50) NOT NULL,
+  `modelo_veiculo` varchar(50) NOT NULL,
+  `descricao_veiculo` varchar(200) NOT NULL,
+  `ano_veiculo` int(4) NOT NULL,
+  `codigo_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `vendedor` (
+  `codigo_vendedor` int(11) NOT NULL,
+  `nome_vendedor` varchar(100) NOT NULL,
+  `cpf_vendedor` bigint(11) NOT NULL,
+  `num_celular` bigint(11) NOT NULL,
+  `codigo_login` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`codigo_usuario`),
+  ADD FOREIGN KEY (`codigo_login`) REFERENCES login(`codigo_login`),
+  ADD UNIQUE KEY `nome_usuario` (`nome_usuario`),
+  ADD UNIQUE KEY `cpf_usuario` (`cpf_usuario`);
+
+ALTER TABLE `veiculo`
+  ADD PRIMARY KEY (`codigo_curso`),
+  ADD FOREIGN KEY (`codigo_usuario`) REFERENCES usuario(`codigo_usuario`);
+
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`codigo_login`),
+  ADD UNIQUE KEY `nome_login` (`nome_login`);
+
+ALTER TABLE `vendedor`
+  ADD PRIMARY KEY (`codigo_vendedor`),
+  ADD FOREIGN KEY (`codigo_login`) REFERENCES login(`codigo_login`),
+  ADD UNIQUE KEY `nome_vendedor` (`nome_vendedor`),
+  ADD UNIQUE KEY `cpf_usuario` (`cpf_usuario`);
+
+ALTER TABLE `usuario`
+  MODIFY `codigo_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `veiculo`
+  MODIFY `codigo_veiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+
+ALTER TABLE `login`
+  MODIFY `codigo_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+
+ALTER TABLE `vendedor`
+  MODIFY `codigo_vendedor` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+INSERT INTO `veiculo` (`codigo_veiculo`, `marca_veiculo`, `modelo_veiculo`,`descricao_veiculo`,`ano_veiculo`,`codigo_usuario`) VALUES
+(1, 'Honda', 'N/A', 'Não há nada ainda', 2003, 0);
+
+INSERT INTO `login` (`codigo_login`, `nome_completo_login`, `nome_login`, `senha_login`, `tipo_login`) VALUES
+(1, 'Administrador', 'admin', '202cb962ac59075b964b07152d234b70', 0),
+(2, 'Aluno', 'aluno', '202cb962ac59075b964b07152d234b70', 1);
+
+
+
+
+
